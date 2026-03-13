28bb792e2eb2695c05487a10ae2e78095102187f
Tobias Vinblad <tobbes7799@gmail.com>
2026-03-13 16:27:12 +0100

    feat: add TopAppBar component and update Home layout with Separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import Calendar from "@/components/WeekCalendar";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import TopAppBar from "@/components/TopAppBar";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <>
+      <TopAppBar />
       <br />
       <Calendar />
+      <Separator className="my-6" />
     </>
   );
 }
