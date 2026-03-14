22a6d99c6e90dfecbbb5330180cbc4c595c1b374
Tobias Vinblad <tobbes7799@gmail.com>
2026-03-14 08:36:00 +0100

    feat: integrate ScrollBar into WorkoutSection for improved scrolling experience

--- src/components/WorkoutSection/index.tsx
+++ src/components/WorkoutSection/index.tsx
@@ -1,18 +1,17 @@
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import WorkoutCard from "./WorkoutCard";
 
 export default function WorkoutSection() {
   return (
-    <div className="flex flex-col items-center">
-      <ScrollArea className="w-full">
-        //TODO: change design of scroll bar
+    <ScrollArea className="w-full">
+      <div className="flex flex-col items-center">
         <WorkoutCard />
         <WorkoutCard />
         <WorkoutCard />
         <WorkoutCard />
         <WorkoutCard />
         <WorkoutCard />
-      </ScrollArea>
-    </div>
+      </div>
+    </ScrollArea>
   );
 }
